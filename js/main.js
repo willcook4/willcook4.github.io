4193426a6d6fab8105e43603d18dc3910ddf7aeb
Will Cook <nzwillcook@gmail.com>
2016-11-16 08:49:47 +0000

    added to js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log("Site coming soon");
+document.addEventListener('DOMContentLoaded', function() {
+  function option1() {
+    console.log('I also like...');
+    console.log('- Chocolate ');
+    console.log('- Beer');
+    console.log('- Penguins');
+    console.log('- Beers');
+    console.log('- Apple Products');
+  }
+  function option2() {
+    console.log('Thanks for the fish!!!');
+  }
+  function option3() {
+    console.log('Remember: ');
+    console.log(' - buy milk');
+    console.log(' - buy bread');
+    console.log(' - buy eggs');
+    console.log(' - email Will');
+  }
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  var a = getRandomInt(1, 3);
+  if (a === 1) {
+    option1();
+  } else if (a === 2) {
+    option2();
+  } else {
+    option3();
+  }
+  console.log('');
+  console.log('Refresh the page to get something new');
+});
