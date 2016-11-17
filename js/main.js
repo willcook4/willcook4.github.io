e83bc5389d0edcdb8b3f7c2976f1f3010c2e9e7c
Will Cook <nzwillcook@gmail.com>
2016-11-17 21:13:30 +0000

    corrected typo

--- js/main.js
+++ js/main.js
@@ -17,19 +17,30 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(' - buy eggs');
     console.log(' - email Will');
   }
+  function option4() {
+    console.log('I liking React at the moment, I\'m doing an online course in it');
+  }
+
+  function option5() {
+    console.log('I\'m having a Moscow mule, what do you want?');
+  }
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  var a = getRandomInt(1, 3);
+  var a = getRandomInt(1, 5);
   if (a === 1) {
     option1();
   } else if (a === 2) {
     option2();
-  } else {
+  } else if (a === 3 ) {
     option3();
+  } else if (a === 4){
+    option4();
+  } else {
+    option5();
   }
   console.log('');
   console.log('Refresh the page to get something new');
